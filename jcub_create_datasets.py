090ddce680731ea52d805f8136c2415d2a24e531
anjandash <anjan8@gmail.com>
2022-03-28 16:58:24 +0200

    added code to make datasets

---
+++ jcub_create_datasets.py
@@ -0,0 +1,21 @@
+# TASKS:
+# _____
+
+# cmpx 
+# mxin
+# name
+# nmlt
+# nmop
+# nmpr
+# nmrt
+# nmtk
+# ntid
+# nuid
+# sloc
+# tloc
+
+# nmcs - CSC [TODO]
+# nmlp - NML [TODO]
+# nmif - NMS [TODO]
+# _____
+
